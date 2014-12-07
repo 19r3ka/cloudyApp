28591a221e8fa34bae9dbba8e8a87734ddfa51c7
19r3ka <yemahiatsi@gmail.com>
2014-12-07 16:29:41 +0100

    Creates the application default layout along with the first custom Bootstrap CSS implementation

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.1.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
+# Bootstrap on rails
+gem 'bootstrap-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-	root 'pages#home'
-	get 'pages/home'
-	get 'pages/help'
-	get 'pages/about'
+	root             'pages#home'
+
+	get 'help'    => 'pages/help'
+	get 'about'   => 'pages/about'
+	get 'contact' => 'pages/contact'
+	
+	get 'signup'  => 'users/new'
+	# get 'login' => 'pages/login'
 
 	resources :cloud_apis
 	resources :users
